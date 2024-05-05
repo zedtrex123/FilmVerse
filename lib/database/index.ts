0391d24ca1a6c24013f8e72d510ed21d2601fb71
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-05-05 23:05:44 +0200

    final version

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -11,7 +11,7 @@ export const connectToDatabase = async () => {
 
   (cached.promise = cached.promise || mongoose.connect(MONGODB_URI)),
     {
-      dbName: 'evently',
+      dbName: 'FilmVerse',
       bufferCommands: false,
     };
 
